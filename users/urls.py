3820d193f3a17dc9b6ed40b84ff936198da039bf
Astro-cmd <mmuichuhia@gmail.com>
2025-02-01 19:06:14 +0300

    modified:   jiranitravels/urls.py 	modified:   users/urls.py

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UserListCreateView, UserDetailView, UserProfileListCreateView, UserProfileDetailView
+
+urlpatterns = [
+    path('users/', UserListCreateView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('profiles/', UserProfileListCreateView.as_view(), name='profile-list-create'),
+    path('profiles/<int:pk>/', UserProfileDetailView.as_view(), name='profile-detail'),
+]
